a163ed019d0f36c4b1b1f5bfb3b8813351bbebd4
Andy <andyfransisko1@gmail.com>
2020-01-27 14:40:28 +0700

    shop UI 1.0

--- application/views/v_signup.php
+++ application/views/v_signup.php
@@ -40,36 +40,11 @@
 
 <body>
   
-
 <!-- Header Start -->
 
-<header class="navigation ">
-	<nav class="navbar navbar-expand-lg main-nav py-lg-3 position-absolute w-100" id="main-nav">
-		<div class="container">
-			<a class="navbar-brand" href="index.html">
-				<img src="<?php echo base_url()?>assets/type1/images/logo.png" width="300px" class="img-fluid">
-			</a>
-
-			<button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navigation"
-				aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="fa fa-bars"></span>
-			</button>
-
-			<div class="collapse navbar-collapse" id="navigation">
-				<ul class="navbar-nav ml-auto">
-					<li class="nav-item active">
-						<a class="nav-link" href="<?php echo base_url()?>Home">Home</a>
-					</li>
-					<li class="nav-item"><a class="nav-link" href="about.html">About</a></li>
-					<li class="nav-item"><a class="nav-link" href="menu.html">Shop</a></li>
-                    <li class="nav-item"><a class="nav-link" href="gallery.html">Forum</a></li>
-                    <li class="nav-item"><a class="nav-link" href="gallery.html">Contact</a></li>
-					<li class="nav-item"><a class="nav-link modal-button">LogIn</a></li>
-				</ul>
-			</div>
-		</div>
-	</nav>
-</header>
+<?php
+	include 'nav.php';
+?>
 
 <!-- Header Close -->
 
