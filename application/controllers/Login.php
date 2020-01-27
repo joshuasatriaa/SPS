3b5b72ad49302f5d7eb97e9bca0a89b343d63f73
calvinfernando <43427639+calvinfernando@users.noreply.github.com>
2020-01-27 14:08:38 +0700

    Login dan m_login

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -2,16 +2,49 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Login extends CI_Controller{
-/*
+
 	function __construct()
 	{
 		parent::__construct();		
 		$this->load->model('m_login');
 	}
-*/		
+		
 	function index()
 	{
 		$this->load->view('v_login');
+	}
+	
+	
+	function login(){
+		$email = $this->input->post('email');
+		$password = $this->input->post('password');
+		$where = array(
+			'email' => $email,
+			'password' => $password
+			);
+		$cek = $this->m_login->cek_login("user", $where)->num_rows();
+		
+		if($cek > 0){
+			$data_session = array(
+				'email' => $email,
+				'password' => $password,
+				'login' => TRUE
+				);
+			$this->session->set_userdata($data_session);
+			
+			//redirect('');
+		}else{
+			echo "email dan password salah !";
+		}	
+	}
+	
+	
+	
+	
+	
+	function logout(){
+		$this->session->sess_destroy();
+		redirect('Login');
 	}
 }
 			
