a544a00a643cf9607823f098dabfc15bbf7b572f
calvinfernando <43427639+calvinfernando@users.noreply.github.com>
2020-02-14 20:51:54 +0700

    after service notification 1.0

---
+++ application/controllers/After_service_notification.php
@@ -0,0 +1,11 @@
+<?php
+class After_service_notification extends CI_Controller{
+	function __construct(){
+		parent::__construct();
+		$this->load->model('m_after_service_notification');
+	}
+	
+	
+		
+}
+?>
---
+++ application/models/m_after_service_notification.php
@@ -0,0 +1,7 @@
+<?php
+class m_after_service_notification extends CI_Model{
+	
+	
+	
+}
+?>
