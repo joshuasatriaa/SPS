c5cafd2394a1469a7eb856c9304f0667b3f816c7
calvinfernando <43427639+calvinfernando@users.noreply.github.com>
2020-02-17 15:11:29 +0700

    after service notif

--- application/controllers/After_service_notification.php
+++ application/controllers/After_service_notification.php
@@ -5,7 +5,38 @@ class After_service_notification extends CI_Controller{
 		$this->load->model('m_after_service_notification');
 	}
 	
+	function index(){
+		
+		
+		
+		$this->load->view('v_after_service_notification');
+	}
 	
 		
+	
+	function email(){
+		
+		$date1 = $this->input->post('waktu_pesanan');
+		$date2 = date("Y-m-d");
+		$diff = date_diff($date1,$date2);
+		
+		if($diff > '3'){
+		
+		$to = $this->input->post('email_pengguna');;
+		$from = "bengcool@gmail.com"
+		$subject = "After Service Notification";
+		$message = "Dari Bengcool";
+		$headers = "From:". $from;
+
+
+		mail($to,$subject,$message,$headers);
+		}
+		
+		
+		
+		
+	}
+	
+	
 }
 ?>
