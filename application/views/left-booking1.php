56e4663d814262629b42a8ac69184e8730482258
Andy <andyfransisko1@gmail.com>
2020-02-28 02:40:37 +0700

    booking UI

---
+++ application/views/left-booking1.php
@@ -0,0 +1,28 @@
+<div class="col-md-3">
+				<div class="category-sidebar">
+				<?php if($this->session->userdata('email') != null) {?>
+					<a href = "<?php echo base_url() ?>Booking/CheckBooking">
+						<div class="widget1 category-list" style="cursor:pointer">
+                            <h4 style="font-family: 'Open Sans', sans-serif;">
+							    <center>All Booking</center>
+						    </h4>
+						</div>
+					</a>
+
+					<a href = "<?php echo base_url() ?>Booking/CurrentBooking">
+						<div class="widget1 category-list" style="cursor:pointer">
+                            <h4 style="font-family: 'Open Sans', sans-serif;">
+							    <center>Current Booking</center>
+						    </h4>
+						</div>
+					</a>
+
+					<div class="widget1 category-list">
+                        <h4 style="font-family: 'Open Sans', sans-serif;">
+						    <center>History</center>
+					    </h4>
+					</div>
+				<?php } ?>
+
+
+				</div>
